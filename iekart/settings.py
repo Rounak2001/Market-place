97f6022f8c2a4370c9e7eab50a9af005e1d41c20
rounak <rounakpatelcp2021@gmail.com>
2024-10-18 15:31:29 +0530

    settings

--- iekart/settings.py
+++ iekart/settings.py
@@ -135,11 +135,11 @@ WHITENOISE_AUTOREFRESH = DEBUG
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
-STATICFILES_DIRS=env.str("STATICFILES_DIRS", default = BASE_DIR/ 'static')
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+#     "/var/www/static/",
+# ]
+STATICFILES_DIRS=[env.str("STATICFILES_DIRS", default = BASE_DIR/"static")]
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = env.str("STATIC_ROOT",default= BASE_DIR/ 'staticfiles')
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
