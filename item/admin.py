dd8e9ea9c17ce5ec944d7a63ccd3619bbe3866b9
rounak <rounakpatelcp2021@gmail.com>
2024-10-18 16:56:16 +0530

    Edited catogries again

--- item/admin.py
+++ item/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Item
+from .models import Item
 
-admin.site.register(Category)
+# admin.site.register(Category)
 admin.site.register(Item)
