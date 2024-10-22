d648b3502a3827b2829754816232c57483eeb0cb
rounak <rounakpatelcp2021@gmail.com>
2024-10-22 09:55:13 +0530

    previuos commited

--- item/models.py
+++ item/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=300)
 
     class Meta:
         ordering = ('name',)
